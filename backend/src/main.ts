37716ffed907ab1cbf94f624ed183773f6d1d83e
“GabrielVScalese” <gabriel.scalese@hotmail.com>
2025-12-03 20:26:01 -0300

    feat: add setup for deploy

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const uploadDir = './uploads';
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+    console.log('Pasta uploads criada com sucesso!');
+  }
+
   app.enableCors({
-    origin: '*',
+    origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
